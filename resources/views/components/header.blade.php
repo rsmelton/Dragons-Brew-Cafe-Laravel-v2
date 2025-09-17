2a86e0a2160d31a4cee5f7b163a56b561253d058
Robert Melton <rsmelton@crimson.ua.edu>
2025-09-17 08:26:22 -0500

    Improved the navbar to some degree, but may make more changes later.

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,6 +1,61 @@
 {{-- New attempt at the navbar, but this time we want two rows of navlinks --}}
+<nav {{ $attributes->merge(['class' => 'w-full h-16 p-2 text-lg bg-opacity-100 sticky top-0']) }}>
+    <div class="flex justify-end gap-2">
+        <x-nav-link :href="route('home')" :active="request()->routeIs('home')">
+            Home
+        </x-nav-link>
 
-{{-- TRY TO CREATE A NEW BRANCH FOR THIS FOR PRACTICE --}}
+        <x-nav-link :href="route('about')" :active="request()->routeIs('about')">
+            About
+        </x-nav-link>
+
+        <x-nav-link :href="route('menu')" :active="request()->routeIs('menu')">
+            Menu
+        </x-nav-link>
+
+        @auth
+            <x-nav-link :href="route('cart')" :active="request()->routeIs('cart')">
+                <i class="fa fa-shopping-cart inline-block text-xl" aria-hidden="true"></i>
+                {{ $cartQuantity }}
+            </x-nav-link>
+        @endauth
+    </div>
+    <div class="flex justify-end gap-2">
+        @guest
+            <x-nav-link :href="route('register')" :active="request()->routeIs('register')">
+                Register
+            </x-nav-link>
+
+            <x-nav-link :href="route('login')" :active="request()->routeIs('login')">
+                Log in
+            </x-nav-link>
+        @endguest
+
+        @auth
+            {{-- <x-nav-link :href="route('cart')" :active="request()->routeIs('cart')">
+                <i class="fa fa-shopping-cart inline-block text-xl" aria-hidden="true"></i>
+                {{ $cartQuantity }}
+            </x-nav-link> --}}
+
+            <x-nav-link :href="route('profile.edit')">
+                {{ __('Profile') }}
+            </x-nav-link>
+
+            <!-- Authentication -->
+            <form method="POST" action="{{ route('logout') }}" class="flex justify-start">
+                @csrf
+
+                <x-nav-link :href="route('logout')"
+                        onclick="event.preventDefault();
+                                    this.closest('form').submit();">
+                    {{ __('Log Out') }}
+                </x-nav-link>
+            </form>
+        @endauth
+
+    </div>
+</nav>
+
 
 {{-- Old code that works, but the nav links are all on the same row--}}
 {{-- <nav {{ $attributes->merge(['class' => 'flex flex-wrap justify-end gap-4 w-full h-16 p-4 text-lg bg-opacity-100 sticky top-0']) }}>
