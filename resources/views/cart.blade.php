fb49ec9dcfa5fbe9533dd405644efaeffbe9f89c
Robert Melton <rsmelton@crimson.ua.edu>
2025-09-29 07:42:32 -0500

    Table is now responsive and doesn't wrap items and the user can scroll on the table instead

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -22,48 +22,80 @@ which is inside my cartHandler.js file --}}
 
         {{-- Otherwise we render out the users cart --}}
         <template x-if="$store.cart.cartTotalQuantity > 0">
-            <div>
+            {{-- <div class="overflow-x-auto sm:overflow-x-visible">
+                <table class="min-w-full border border-gray-200 divide-y divide-gray-200">
+                    <thead class="bg-gray-100">
+                        <tr>
+                            <th class="px-4 py-2 text-left text-sm font-medium text-gray-700 whitespace-nowrap">Name</th>
+                            <th class="px-4 py-2 text-left text-sm font-medium text-gray-700 whitespace-nowrap">Email</th>
+                            <th class="px-4 py-2 text-left text-sm font-medium text-gray-700 whitespace-nowrap">Role</th>
+                            <th class="px-4 py-2 text-left text-sm font-medium text-gray-700 whitespace-nowrap">Status</th>
+                            <th class="px-4 py-2 text-left text-sm font-medium text-gray-700 whitespace-nowrap">Created At</th>
+                        </tr>
+                    </thead>
+                    <tbody class="bg-white divide-y divide-gray-200">
+                        <tr>
+                            <td class="px-4 py-2 whitespace-nowrap">John Doe</td>
+                            <td class="px-4 py-2 whitespace-nowrap">john@example.com</td>
+                            <td class="px-4 py-2 whitespace-nowrap">Admin</td>
+                            <td class="px-4 py-2 whitespace-nowrap">Active</td>
+                            <td class="px-4 py-2 whitespace-nowrap">2025-09-26</td>
+                        </tr>
+                        <tr>
+                            <td class="px-4 py-2 whitespace-nowrap">Jane Smith</td>
+                            <td class="px-4 py-2 whitespace-nowrap">jane@example.com</td>
+                            <td class="px-4 py-2 whitespace-nowrap">User</td>
+                            <td class="px-4 py-2 whitespace-nowrap">Inactive</td>
+                            <td class="px-4 py-2 whitespace-nowrap">2025-09-20</td>
+                        </tr>
+                    <!-- more rows -->
+                    </tbody>
+                </table>
+            </div> --}}
+
+            <div class="overflow-x-auto sm:overflow-x-visible">
                 <table class="min-w-full divide-y divide-gray-200 text-sm text-left">
                     <thead class="bg-gray-100 text-gray-600 uppercase text-xs">
                         <tr>
-                            <th class="px-6 py-3">Change Quantity</th>
-                            <th class="px-6 py-3">Item Name</th>
-                            <th class="px-6 py-3">Price per item</th>
-                            <th class="px-6 py-3">Quantity</th>
-                            <th class="px-6 py-3 text-right">Cost</th>
+                            <th class="px-6 py-3 whitespace-nowrap">Change Quantity</th>
+                            <th class="px-6 py-3 whitespace-nowrap">Item Name</th>
+                            <th class="px-6 py-3 whitespace-nowrap">Price per item</th>
+                            <th class="px-6 py-3 whitespace-nowrap">Quantity</th>
+                            <th class="px-6 py-3 text-right whitespace-nowrap">Cost</th>
                         </tr>
                     </thead>
                     <tbody class="divide-y divide-gray-200 bg-gray-300">
                         <template x-for="cartItem in $store.cart.cartItems" :key="cartItem.id">
                             <tr class="hover:bg-gray-50">
                                 <td class="px-6 py-4">
-                                    <div class="flex flex-wrap items-center gap-2">
+                                    <div class="flex flex-shrink-0 items-center gap-2 whitespace-nowrap">
                                         {{-- If there is only 1 of a cart item left then we display 
                                         the trash can icon to allow it to be fully removed --}}
                                         <template x-if="cartItem.quantity === 1">
-                                            <button @click="$store.cart.deleteFromCart(cartItem.id)" class="bg-red-500 text-blue-800 px-2 py-1 rounded text-xs">
-                                                <img style="width: 2.5rem; height: 2.5rem;" src="/images/trashcan-icon.png" alt="Decrease quantity from cart button">
+                                            <button @click="$store.cart.deleteFromCart(cartItem.id)" class="bg-red-500 text-blue-800 px-2 py-1 rounded text-xs whitespace-nowrap">
+                                                <img class="w-8 h-auto min-w-8" src="/images/trashcan-icon.png" alt="Decrease quantity from cart button">
                                             </button>
                                         </template>
                                         {{-- If the quantity of the cart item is more than 1 then we can 
                                         display a minus button to allow the item to be decremented --}}
                                         <template x-if="cartItem.quantity > 1">
-                                            <button @click="$store.cart.decrementQuantity(cartItem.id)" class="bg-red-500 text-blue-800 px-2 py-1 rounded text-xs">
-                                                <img style="width: 2.5rem; height: 2.5rem;" src="/images/minus-icon.png" alt="Decrease quantity from cart button">
+                                            <button @click="$store.cart.decrementQuantity(cartItem.id)" class="bg-red-500 text-blue-800 px-2 py-1 rounded text-xs whitespace-nowrap">
+                                                <img class="w-8 h-auto min-w-8" src="/images/minus-icon.png" alt="Decrease quantity from cart button">
                                             </button>
                                         </template>
                                         {{-- Display the quantity of the current cart item --}}
-                                        <span x-text="cartItem.quantity" class="text-black px-4 py-1 rounded text-xl"></span>
+                                        <span x-text="cartItem.quantity" class="text-black px-4 py-1 rounded text-xl whitespace-nowrap"></span>
                                         {{-- Display a plus icon to allow items to be added to the cart --}}
-                                        <button @click="$store.cart.incrementQuantity(cartItem.id)" class="bg-green-400 text-blue-800 px-2 py-1 rounded text-xs">
-                                            <img style="width: 2.5rem; height: 2.5rem;" src="/images/plus-icon.png" alt="Increase quantity from cart button">
+                                        <button @click="$store.cart.incrementQuantity(cartItem.id)" class="bg-green-400 text-blue-800 px-2 py-1 rounded text-xs whitespace-nowrap">
+                                            <img class="w-8 h-auto min-w-8" src="/images/plus-icon.png" alt="Increase quantity from cart button">
                                         </button>
+                                        {{-- <span x-text="cartItem.quantity" class="text-black px-4 py-1 rounded text-xl"></span> --}}
                                     </div>
                                 </td>
-                                <td x-text="cartItem.menuItem.name" class="px-6 py-4 font-medium text-gray-800"></td>
-                                <td x-text="`$${cartItem.menuItem.price}`" class="px-6 py-4 text-gray-600"></td>
-                                <td x-text="cartItem.quantity" class="px-6 py-4 text-gray-800"></td>
-                                <td x-text="`$${(cartItem.menuItem.price * cartItem.quantity).toFixed(2)}`" class="px-6 py-4 text-gray-800 text-right space-x-2"></td>
+                                <td x-text="cartItem.menuItem.name" class="px-6 py-4 font-medium text-gray-800 whitespace-nowrap"></td>
+                                <td x-text="`$${cartItem.menuItem.price}`" class="px-6 py-4 text-gray-600 whitespace-nowrap"></td>
+                                <td x-text="cartItem.quantity" class="px-6 py-4 text-gray-800 whitespace-nowrap"></td>
+                                <td x-text="`$${(cartItem.menuItem.price * cartItem.quantity).toFixed(2)}`" class="px-6 py-4 text-gray-800 text-right space-x-2 whitespace-nowrap"></td>
                             </tr>
                         </template>
                     </tbody>
