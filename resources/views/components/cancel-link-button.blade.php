017bff40b2bc1cab01c8806da87348a96b818606
Robert Melton <rsmelton@crimson.ua.edu>
2025-11-27 02:49:57 -0600

    Added cancel link button to register and login pages

---
+++ resources/views/components/cancel-link-button.blade.php
@@ -0,0 +1,3 @@
+<a href="/" class="inline-flex items-center px-4 py-2 mr-auto bg-red-400 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-500 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
+    Cancel
+</a>
